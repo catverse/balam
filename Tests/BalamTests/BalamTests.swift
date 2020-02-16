74f76a1dfa7c2a47a1165038dd147976b68464ee
vauxhall <zero.griffin@gmail.com>
2020-02-16 15:02:29 +0100

    Clean project

--- Tests/BalamTests/BalamTests.swift
+++ Tests/BalamTests/BalamTests.swift
@@ -3,13 +3,6 @@ import XCTest
 
 final class BalamTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
         XCTAssertEqual(Balam().text, "Hello, World!")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import BalamTests
-
-var tests = [XCTestCaseEntry]()
-tests += BalamTests.allTests()
-XCTMain(tests)
