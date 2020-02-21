739b4b3d682d8c04ade2ea6eefcbc8b4afa61213
vauxhall <zero.griffin@gmail.com>
2020-02-21 21:47:57 +0100

    Add more properties

--- Sources/Balam/Property.swift
+++ Sources/Balam/Property.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public class Property: Codable, Hashable {
-    private func equals(_ property: Property) -> Bool {
-        type(of: self) == type(of: property)
+    public func hash(into: inout Hasher) {
+        into.combine(Swift.String(describing: type(of: self)))
     }
     
-    public func hash(into: inout Hasher) {
-//        into.combine(name)
+    private func equals(_ property: Property) -> Bool {
+        type(of: self) == type(of: property)
     }
     
     public static func == (lhs: Property, rhs: Property) -> Bool {
@@ -24,6 +24,11 @@ public class Property: Codable, Hashable {
         required init(from: Decoder) throws {
             try name = from.container(keyedBy: Key.self).decode(Swift.String.self, forKey: .name)
             try super.init(from: from)
+        }
+        
+        public override func hash(into: inout Hasher) {
+            super.hash(into: &into)
+            into.combine(name)
         }
         
         override func equals(_ property: Property) -> Bool {
@@ -48,6 +53,11 @@ public class Property: Codable, Hashable {
             try super.init(from: from)
         }
         
+        public override func hash(into: inout Hasher) {
+            super.hash(into: &into)
+            into.combine(property)
+        }
+        
         override func equals(_ property: Property) -> Bool {
             super.equals(property) && self.property == (property as! Wrap).property
         }
@@ -58,55 +68,43 @@ public class Property: Codable, Hashable {
     }
     
     public final class Optional: Wrap { }
+    public final class Array: Wrap { }
+    public final class Set: Wrap { }
     public final class String: Concrete { }
     public final class Int: Concrete { }
-}
-
-/*
-final class Property: Codable, Hashable {
-    var dictionary: Dict?
-    var group = Group.one
-    var optional = false
-    let name: String
-    let value: Value
+    public final class Double: Concrete { }
+    public final class Float: Concrete { }
     
-    struct Dict: Codable, Hashable {
-        let key: Property
-        let value: Property
-    }
-    
-    enum Group: String, Codable {
-        case
-        one,
-        array,
-        dictionary,
-        set
-    }
-    
-    enum Value: String, Codable {
-        case
-        _struct,
-        _class,
-        _enum,
-        string,
-        int,
-        double
-    }
-    
-    func hash(into: inout Hasher) {
-        into.combine(name)
-        into.combine(value)
-        into.combine(optional)
-        into.combine(group)
-        dictionary.map { into.combine($0) }
-    }
-    
-    static func == (lhs: Property, rhs: Property) -> Bool {
-        lhs.name == rhs.name
-            && lhs.value == rhs.value
-            && lhs.optional == rhs.optional
-            && lhs.group == rhs.group
-            && lhs.dictionary == rhs.dictionary
+    public final class Dictionary: Concrete {
+        public let key: Property
+        public let value: Property
+        
+        init(_ name: Swift.String, key: Property, value: Property) {
+            self.key = key
+            self.value = value
+            super.init(name)
+        }
+        
+        required init(from: Decoder) throws {
+            try key = from.container(keyedBy: Key.self).decode(Property.self, forKey: .key)
+            try value = from.container(keyedBy: Key.self).decode(Property.self, forKey: .value)
+            try super.init(from: from)
+        }
+        
+        public override func hash(into: inout Hasher) {
+            super.hash(into: &into)
+            into.combine(key)
+            into.combine(value)
+        }
+        
+        override func equals(_ property: Property) -> Bool {
+            super.equals(property) && self.key == (property as! Dictionary).key && self.value == (property as! Dictionary).value
+        }
+        
+        private enum Key: CodingKey {
+            case
+            key,
+            value
+        }
     }
 }
-*/
--- Tests/Tests/PropertyTests.swift
+++ Tests/Tests/PropertyTests.swift
@@ -10,5 +10,21 @@ final class PropertyTests: XCTestCase {
         XCTAssertEqual(Property.Optional(Property.String("a")), Property.Optional(Property.String("a")))
         XCTAssertNotEqual(Property.Optional(Property.String("a")), Property.String("a"))
         XCTAssertNotEqual(Property.Optional(Property.String("a")), Property.Optional(Property.String("b")))
+        XCTAssertEqual(Property.Array(Property.String("a")), Property.Array(Property.String("a")))
+        XCTAssertNotEqual(Property.Array(Property.String("a")), Property.Array(Property.String("b")))
+        XCTAssertNotEqual(Property.Array(Property.String("a")), Property.Optional(Property.String("a")))
+        XCTAssertEqual(Property.Optional(Property.Array(Property.String("a"))), Property.Optional(Property.Array(Property.String("a"))))
+        XCTAssertNotEqual(Property.Optional(Property.Array(Property.String("a"))), Property.Optional(Property.Array(Property.String("b"))))
+        XCTAssertNotEqual(Property.Array(Property.Optional(Property.String("a"))), Property.Optional(Property.Array(Property.String("a"))))
+        XCTAssertEqual(Property.Dictionary("a", key: Property.String(""), value: Property.Int("")), Property.Dictionary("a", key: Property.String(""), value: Property.Int("")))
+        XCTAssertNotEqual(Property.Dictionary("a", key: Property.String(""), value: Property.Int("")), Property.Dictionary("b", key: Property.String(""), value: Property.Int("")))
+    }
+    
+    func testHashable() {
+        XCTAssertEqual(1, Set<Property>([Property.String("a"), Property.String("a"), Property.String("a")]).count)
+        XCTAssertEqual(2, Set<Property>([Property.String("a"), Property.Int("a")]).count)
+        XCTAssertEqual(2, Set<Property>([Property.String("a"), Property.String("b")]).count)
+        XCTAssertEqual(2, Set<Property>([Property.String("a"), Property.Optional(Property.String("a"))]).count)
+        XCTAssertEqual(1, Set<Property>([Property.Optional(Property.String("a")), Property.Optional(Property.String("a"))]).count)
     }
 }
