b1ea42eed988e572ed324aeb4b504cf5c26c55ad
vauxhall <zero.griffin@gmail.com>
2020-02-23 13:44:34 +0100

    Parse wrapped

--- Sources/Balam/Mirror.swift
+++ Sources/Balam/Mirror.swift
@@ -4,22 +4,25 @@ extension Mirror {
     func reflect<T>(_ object: T) -> Set<Property> where T : Codable {
         children.reduce(into: .init()) {
             $0.insert(isNil($1.1)
-                ? guess($1.0!, object: object)
+                ? .Optional(guess($1.0!, object: object))
                 : {
                     $1 == .optional ? .Optional($0) : $0
-                } (child($1.0!, value: $1.1), Mirror(reflecting: $1.1).displayStyle))
+                } (isEmpty($1.1) ? guess($1.0!, object: object) : wrapped($1.0!, value: $1.1), Mirror(reflecting: $1.1).displayStyle))
         }
     }
     
     private func guess<T>(_ property: String, object: T) -> Property where T : Codable {
-        for value in ["", 0, false] as [Any] {
+        ["", 0, false, [""], [0], [false]].compactMap {
             var json = try! JSONSerialization.jsonObject(with: JSONEncoder().encode(object)) as! [String : Any]
-            json[property] = value
-            if let decoded = try? JSONDecoder().decode(T.self, from: JSONSerialization.data(withJSONObject: json)) {
-                return .Optional(child(property, value: Mirror(reflecting: decoded).children.first { $0.label == property }!.value))
+            json[property] = $0
+            return (try? JSONDecoder().decode(T.self, from: JSONSerialization.data(withJSONObject: json))).map {
+                Mirror(reflecting: $0).children.first { $0.label == property }!.value
             }
-        }
-        return .Optional(.Custom(property))
+        }.first.map { wrapped(property, value: $0) } ?? .Custom(property)
+    }
+    
+    private func wrapped(_ name: String, value: Any) -> Property {
+        value is [Any] ? .Array(child(name, value: (value as! [Any]).first!)) : child(name, value: value)
     }
     
     private func child(_ name: String, value: Any) -> Property {
@@ -28,9 +31,12 @@ extension Mirror {
         case is Int: return .Int(name)
         case is Double: return .Double(name)
         case is Bool: return .Boolean(name)
-        case is [String]: return .Array(.String(name))
         default: return .Custom(name)
         }
+    }
+    
+    private func isEmpty(_ value: Any) -> Bool {
+        value is [Any] ? (value as! [Any]).first.map { isNil($0) } ?? true : false
     }
     
     private func isNil(_ value: Any) -> Bool {
--- Tests/Tests/NodeTests.swift
+++ Tests/Tests/NodeTests.swift
@@ -5,18 +5,25 @@ import Combine
 @available(OSX 10.15, *) final class NodeTests: XCTestCase {
     func testParse() {
         struct Model: Codable {
-            let arrString = [String]()
-            let arrStringOptional = [String?]()
-            let opString: String? = "opt"
-            let opInt: Int? = 9
-            let name = "Lorem Ipsum"
-            let age = 99
-            let phone = 2.3
-            let myBool = false
+            var arrString = [String]()
+            var arrStringOptional = [String?]()
+            var opString: String? = "opt"
+            var opInt: Int? = 9
+            var name = "Lorem Ipsum"
+            var age = 99
+            var phone = 2.3
+            var myBool = false
+            var arrInt = [Int]()
+            var arrDouble = [Double]()
+            var arrStringOp: [String]? = []
             var opNilString: String? = nil
             var opNilInt: Int? = nil
             var opNilDouble: Double? = nil
             var opNilBool: Bool? = nil
+            var arrStringNil: [String]?
+            var arrIntNil: [Int]?
+            var arrDoubleNil: [Double]?
+            var arrBoolNil: [Bool]?
         }
         
         let node = Node(Model())
@@ -33,5 +40,12 @@ import Combine
         XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { ($0.property as? Property.Int)?.name == "opNilInt" })
         XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { ($0.property as? Property.Double)?.name == "opNilDouble" })
         XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { ($0.property as? Property.Boolean)?.name == "opNilBool" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { (($0.property as? Property.Array)?.property as? Property.String)?.name == "arrStringOp" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { (($0.property as? Property.Array)?.property as? Property.String)?.name == "arrStringNil" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Array }.first { ($0.property as? Property.Int)?.name == "arrInt" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { (($0.property as? Property.Array)?.property as? Property.Int)?.name == "arrIntNil" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Array }.first { ($0.property as? Property.Double)?.name == "arrDouble" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { (($0.property as? Property.Array)?.property as? Property.Double)?.name == "arrDoubleNil" })
+        XCTAssertNotNil(node.properties.compactMap { $0 as? Property.Optional }.first { (($0.property as? Property.Array)?.property as? Property.Boolean)?.name == "arrBoolNil" })
     }
 }
