d99ec47f6d805817c92f4ea6675b496c5dce4ac7
vauxhall <zero.griffin@gmail.com>
2020-02-29 11:22:53 +0100

    Add serial list

--- Sources/Balam/Serialiser.swift
+++ Sources/Balam/Serialiser.swift
@@ -4,7 +4,7 @@ struct Serialiser {
     static func encode(_ container: UnkeyedEncodingContainer, properties: Set<Property>) {
         var container = container
         properties.forEach { property in
-            try! container.encode(list.firstIndex { $0 == type(of: property) }!)
+            try! container.encode(UInt8(list.firstIndex { $0 == type(of: property) }!))
             try! container.encode(property)
         }
     }
@@ -13,12 +13,30 @@ struct Serialiser {
         var set = Set<Property>()
         var container = container
         while !container.isAtEnd {
-            try! set.insert(container.decode(list[container.decode(Int.self)]))
+            try! set.insert(container.decode(list[.init(container.decode(UInt8.self))]))
         }
         return set
     }
     
     private static let list = [
+        Property.Custom.self,
+        Property.Optional.self,
+        Property.Array.self,
+        Property.Set.self,
         Property.String.self,
-        Property.Double.self]
+        Property.Int.self,
+        Property.Double.self,
+        Property.Float.self,
+        Property.Boolean.self,
+        Property.Date.self,
+        Property.Data.self,
+        Property.UInt8.self,
+        Property.UInt16.self,
+        Property.UInt32.self,
+        Property.UInt64.self,
+        Property.Int8.self,
+        Property.Int16.self,
+        Property.Int32.self,
+        Property.Int64.self,
+        Property.Dictionary.self]
 }
